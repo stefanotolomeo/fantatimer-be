8795acf577fb60cac77bf08b476c8a44ddc40cf8
Stefano Tolomeo <stefano.tolomeo.adv@bancacfplus.it>
2023-09-01 17:48:38 +0200

    ConfigProd

--- src/config/config_prod.js
+++ src/config/config_prod.js
@@ -1,14 +1,13 @@
 var config = {};
 
-const root = "C:/EDOO"
-config.videos = root + "/resources/videos/"
-config.sounds = root + "/resources/sounds/"
-config.images = root + "/resources/images/"
+config.videos = __dirname + "/../../../edoo-resources/videos/"
+config.sounds = __dirname + "/../../../edoo-resources/sounds/"
+config.images = __dirname + "/../../../edoo-resources/images/"
 
-config.player = root + "/viewer/"
+config.player = __dirname + "/../../../edoo-viewer/src"
 
-config.privateKey = root + "/server/ssl/key.pem"
-config.certificate = root + "/server/ssl/cert.pem"
+config.privateKey = __dirname + "/../ssl/key.pem"
+config.certificate = __dirname + "/../ssl/cert.pem"
 
 config.httpPort = 2999;
 config.httpsPort = 3000;
