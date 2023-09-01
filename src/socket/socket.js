968af8684f7f1d86fcdfa283aa96524d38d916f4
Stefano Tolomeo <stefano.tolomeo.adv@bancacfplus.it>
2023-09-01 18:26:10 +0200

    Set origin=true to CORS

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -9,7 +9,7 @@ module.exports = class SocketServer {
     this.io = require("socket.io")(http, {
       cors: {
         //origin: ["http://127.0.0.1:3000", "http://127.0.0.1:8080"],
-        origin: "*",
+        origin: true,
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         preflightContinue: false,
         allowedHeaders: ["fantatimer-socket-header"],
