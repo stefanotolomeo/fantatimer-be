7133e2c9de0e1bdc7c91bbb69d5e0811a7dfae8e
Stefano Tolomeo <stefano.tolomeo.adv@bancacfplus.it>
2023-09-01 20:39:16 +0200

    Update logins

--- src/model/State.js
+++ src/model/State.js
@@ -1,13 +1,13 @@
 const Player = require("./Player")
 
-const PLAYER_1 = Player("tolo", "gia", "aaa")
-const PLAYER_2 = Player("ivan", "sen", "bbb")
-const PLAYER_3 = Player("fra", "sky", "ccc")
-const PLAYER_4 = Player("dennis", "rea", "ddd")
-const PLAYER_5 = Player("ema", "vig", "eee")
-const PLAYER_6 = Player("cari", "ich", "fff")
-const PLAYER_7 = Player("nico", "rob", "ggg")
-const PLAYER_8 = Player("davide", "don", "eee")
+const PLAYER_1 = Player("tolo", "giavazzi", "dispiace")
+const PLAYER_2 = Player("ivan", "sentenza", "paracarri123")
+const PLAYER_3 = Player("fra", "skymagic", "haigiavinto")
+const PLAYER_4 = Player("dennis", "realsedriano", "fcmilan1")
+const PLAYER_5 = Player("ema", "vighignolo", "pupilli")
+const PLAYER_6 = Player("cari", "ichiuforta", "lautaro300")
+const PLAYER_7 = Player("nico", "robbdematt", "anchesimpaticamente")
+const PLAYER_8 = Player("davide", "donandres", "fcmilan2")
 
 const State = () => {
     return {
